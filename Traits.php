fc73e75a595fd7416d4bafacb607282a80507c2e
Ayon Ghosh <103620492+Ayon04@users.noreply.github.com>
2024-10-23 17:52:21 +0600

    Add files via upload

---
+++ Traits.php
@@ -0,0 +1,52 @@
+<?php
+class  User {
+    
+    public function setUserID(){
+
+                                 
+    }
+
+    public function getUserID(){
+
+        
+    }
+
+}
+
+
+trait Human{
+  
+    public function setName(){
+
+                                 
+    }
+
+    public function getName(){
+
+        
+    }
+
+
+
+
+}
+
+
+
+
+
+
+class Student extends User {
+
+
+
+}
+
+
+
+
+
+
+
+
+?>
