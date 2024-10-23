5a16cfc6b7307c03e9e131be70fb2f10a99c684b
Ayon Ghosh <103620492+Ayon04@users.noreply.github.com>
2024-10-23 17:53:28 +0600

    Add files via upload

--- Traits.php
+++ Traits.php
@@ -1,50 +1,97 @@
 <?php
 class  User {
-    
-    public function setUserID(){
 
+    public $userID;
+
+    
+    public function setUserID($id){
+          
+        $this->userID = $id;
                                  
     }
 
     public function getUserID(){
 
-        
+        return $this->userID;
     }
 
 }
 
 
 trait Human{
-  
-    public function setName(){
 
-                                 
+    public $name; 
+  
+    public function setName($nm){
+  
+         $this->name = $nm;
+        
     }
 
     public function getName(){
+      
 
+        return  $this -> name;
         
     }
 
 
-
-
 }
 
 
+trait Activits{
+
+   
+
+    public function getActivites(){
+      
+
+        return  'Studying,Sleeping,Eating,Watching Movies,Playing Games';
+        
+    }
 
 
+}
 
 
 class Student extends User {
 
+use Human,Activits;
+
+public function getClassname(){
+
+
+
+     return 'Student Class';
+}
 
 
 }
 
 
+$stu = new Student();
+
+$stu->setName('Akash Kumar Ghosh');
+echo $stu->getName();
+
+echo '<br>';
+echo '<br>';
 
 
+$stu-> setUserID(3445345);
+echo $stu ->getUserID();
+
+
+echo '<br>';
+echo '<br>';
+
+
+echo $stu ->getActivites();
+
+echo '<br>';
+echo '<br>';
+
+echo $stu ->getClassname();
 
 
 
